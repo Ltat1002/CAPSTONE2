76f8305fa3b80943c5ee5f0665e8a0ec285bd205
Giabao67 <https://Giabao67@github.com>
2023-04-01 21:53:36 +0700

    fix coflict

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :users
+=======
+  resources :vouchers
+>>>>>>> ca02fe0 (modify db user, report and add db vouchers)
   resources :reviews
   resources :reports
   resources :users
   resources :repair_equipments
-  resources :wards
-  resources :districts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- backend/test/models/voucher_test.rb
+++ backend/test/models/voucher_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class DistrictTest < ActiveSupport::TestCase
+class VoucherTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
