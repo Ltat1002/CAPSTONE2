ef9f7ea6427e969b99ba656dd3a27f200b86c88d
Giabao67 <https://Giabao67@github.com>
2023-04-01 22:01:37 +0700

    fix conflict file

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
   devise_for :users
-=======
-=======
-  devise_for :users
->>>>>>> 9089f07 (add devise for users and active storage for reports)
   resources :vouchers
->>>>>>> ca02fe0 (modify db user, report and add db vouchers)
   resources :reviews
   resources :reports
   resources :users
