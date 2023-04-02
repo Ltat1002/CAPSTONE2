703a1853690f76529ab0f767067ec2be64dfe74f
TungNguyen304 <nguyenvantung2001dn@gmail.com>
2023-04-02 21:43:34 +0700

    nothing

--- frontend/src/views/auth/AuthenticationManager.vue
+++ frontend/src/views/auth/AuthenticationManager.vue
@@ -1,7 +1,10 @@
 <template lang="">
   <div>
     <router-view></router-view>
+    <div v-for="value in data" :key="value.value">{{ value.value }}</div>
   </div>
 </template>
-<script></script>
+<script setup>
+const data = [{ value: "Name" }];
+</script>
 <style lang=""></style>
