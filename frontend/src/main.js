d81a2a2e76f7ae0838b691c24be85765416d906b
TungNguyen304 <nguyenvantung2001dn@gmail.com>
2023-04-05 08:18:44 +0700

    UI login, register

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { http } from "./configs/httpCommon";
 import PrimeVue from "primevue/config";
 import "./index.scss";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
@@ -9,4 +10,5 @@ import "primeicons/primeicons.css";
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue);
+app.use(http);
 app.mount("#app");
