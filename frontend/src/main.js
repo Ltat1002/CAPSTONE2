f0abc6ecdfbe18bd3bb74aff82d9f9107f01420f
Mark <81944563+TungNguyen304@users.noreply.github.com>
2023-04-08 22:01:33 +0700

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { http } from "./configs/httpCommon";
 import PrimeVue from "primevue/config";
 import "./index.scss";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
@@ -12,5 +11,4 @@ import "boxicons";
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue);
-app.use(http);
 app.mount("#app");
