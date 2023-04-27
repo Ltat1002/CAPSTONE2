bb6a56871c0196c4e3ce4e263a63f044f7884038
Giabao67 <lenguyengiabao6701@gmail.com>
2023-04-27 11:57:48 +0700

    add seed repair equipments

--- backend/app/controllers/api/v1/repair_equipments_controller.rb
+++ backend/app/controllers/api/v1/repair_equipments_controller.rb
@@ -1,42 +1,40 @@
 class Api::V1::RepairEquipmentsController < ApplicationController
-  before_action :set_repair_equipment, only: %i[ show update destroy ]
+  before_action :set_repair_equipment, only: :show
 
-  # GET /repair_equipments
   def index
-    @repair_equipments = RepairEquipment.all
+    @repair_equipments = RepairEquipment.with_attached_photo
 
-    render json: @repair_equipments
+    render json: @repair_equipments.as_json(methods: :photo_url)
   end
 
-  # GET /repair_equipments/1
   def show
-    render json: @repair_equipment
+    render json: @repair_equipment.as_json.merge(photo: url_for(@repair_equipment.photo))
   end
 
-  # POST /repair_equipments
-  def create
-    @repair_equipment = RepairEquipment.new(repair_equipment_params)
+  # # POST /repair_equipments
+  # def create
+  #   @repair_equipment = RepairEquipment.new(repair_equipment_params)
 
-    if @repair_equipment.save
-      render json: @repair_equipment, status: :created, location: @repair_equipment
-    else
-      render json: @repair_equipment.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @repair_equipment.save
+  #     render json: @repair_equipment, status: :created, location: @repair_equipment
+  #   else
+  #     render json: @repair_equipment.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # PATCH/PUT /repair_equipments/1
-  def update
-    if @repair_equipment.update(repair_equipment_params)
-      render json: @repair_equipment
-    else
-      render json: @repair_equipment.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /repair_equipments/1
+  # def update
+  #   if @repair_equipment.update(repair_equipment_params)
+  #     render json: @repair_equipment
+  #   else
+  #     render json: @repair_equipment.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /repair_equipments/1
-  def destroy
-    @repair_equipment.destroy
-  end
+  # # DELETE /repair_equipments/1
+  # def destroy
+  #   @repair_equipment.destroy
+  # end
 
   private
 
@@ -45,6 +43,6 @@ class Api::V1::RepairEquipmentsController < ApplicationController
   end
 
   def repair_equipment_params
-    params.permit(:name, :description)
+    params.permit(:name, :description, :photo)
   end
 end
